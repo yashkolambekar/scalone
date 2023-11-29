e279e7a7bebdbf1cae9905643ed0c31be63cf3e8
Yash Kolambekar <yashskolambekar2015@gmail.com>
2023-11-29 19:40:07 +0530

    first commit

---
+++ components/nav/nav.php
@@ -0,0 +1,3 @@
+<div id="nav">
+    <p>scalone</p>
+</div>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SCAL</title>
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <?php include "components/nav/nav.php" ?>
+
+    <section id="currentandgraph">
+        <div id="current_wrapper">
+            <p id='current_weight'>45</p>
+        </div>
+    </section>
+
+</body>
+</html>
